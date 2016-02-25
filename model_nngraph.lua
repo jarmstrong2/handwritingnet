cd4da63d95f0ba1cbd7ca7b1e95eb6327a9e1ae8
John Armstrong <j2armstrong@gmail.com>
2016-02-25 12:46:40 -0500

    Update model_nngraph.lua

--- model_nngraph.lua
+++ model_nngraph.lua
@@ -13,21 +13,6 @@ local model_utils=require 'model_utils'
 require 'cunn'
 require 'distributions'
 torch.manualSeed(123)
-local cmd = torch.CmdLine()
-cmd:text()
-cmd:text('Script for training sequence model.')
-
-cmd:option('-inputSize' , 3, 'number of input dimension')
-cmd:option('-hiddenSize' , 400, 'number of hidden units in lstms')
-cmd:option('-lr' , 1e-4, 'learning rate')
-cmd:option('-maxlen' , 100, 'max sequence length')
-cmd:option('-batchSize' , 4, 'mini batch size')
-cmd:option('-numPasses' , 1, 'number of passes')
-cmd:option('-valData' , 'toy.asc', 'filepath for validation data')
-cmd:option('-trainData' , 'toy.asc', 'filepath for training data')
-
-cmd:text()
-opt = cmd:parse(arg)
 
 -- get training dataset
 dataFile = torch.DiskFile(opt.trainData, 'r')
